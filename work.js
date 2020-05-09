45fc08075d0616d7e3c4bae73d9e4340b8ae04e9
kylelambert <kylelambertdev@gmail.com>
2020-05-09 18:38:49 -0400

    adjusted prev/next arrows on slideshow

--- work.js
+++ work.js
@@ -16,13 +16,13 @@ window.addEventListener("load",function() {
 // NEXT AND PREVIOUS CONTROL
 function plusSlides(n){
     clearInterval(myTimer);
-    if (n < 0){
+    if (n < 0) {
         showSlides(slideIndex -= 1);
     } else {
         showSlides(slideIndex += 1); 
     }
   
-    if (n === -1){
+    if (n === -1) {
         myTimer = setInterval(function(){plusSlides(n + 2)}, timeToChange);
     } else {
         myTimer = setInterval(function(){plusSlides(n + 1)}, timeToChange);
@@ -30,13 +30,13 @@ function plusSlides(n){
 }
 
 //Controls the current slide and resets interval if needed
-function currentSlide(n){
+function currentSlide(n) {
     clearInterval(myTimer);
     myTimer = setInterval(function(){plusSlides(n + 1)}, timeToChange);
     showSlides(slideIndex = n);
 }
 
-function showSlides(n){
+function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     if (n > slides.length) {slideIndex = 1}
