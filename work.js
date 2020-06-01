52d6e07a713d5864cfffe1df341a9c674842c400
kylelambert <kylelambertdev@gmail.com>
2020-06-01 14:49:42 -0400

    multiple slideshows on work page

--- work.js
+++ work.js
@@ -1,57 +1,65 @@
-var slideIndex = 1;
+var slideIndex;
 var myTimer;
-var slideshowContainer;
 var timeToChange = 3000;
+var slideshows, numSlideshows;
 
 window.addEventListener("load",function() {
-    showSlides(slideIndex);
-    myTimer = setInterval(function(){plusSlides(1)}, timeToChange);
+
+    slideshows = document.getElementsByClassName("slideshow-inner");
+    numSlideshows = slideshows.length;
+    slideIndex = [];
+    myTimer = [];
+    console.log(slideshows.length);
   
-    slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
-  
-    slideshowContainer.addEventListener('mouseenter', pause)
-    slideshowContainer.addEventListener('mouseleave', resume)
+    for (var i = 0; i < numSlideshows; i++)
+    {
+        slideIndex[i] = 1;
+        showSlides(slideIndex[i], i);
+        //myTimer[i] = setInterval(function(){plusSlides(1, i)}, timeToChange);
+
+        // slideshows[i].addEventListener('mouseenter', pause(i))
+        // slideshows[i].addEventListener('mouseleave', resume(i))
+    }
 })
 
 // NEXT AND PREVIOUS CONTROL
-function plusSlides(n){
-    clearInterval(myTimer);
+function plusSlides(n, i){
+    //clearInterval(myTimer[i]);
     if (n < 0) {
-        showSlides(slideIndex -= 1);
+        showSlides(slideIndex[i] -= 1, i);
     } else {
-        showSlides(slideIndex += 1); 
+        showSlides(slideIndex[i] += 1, i); 
     }
   
-    if (n === -1) {
-        myTimer = setInterval(function(){plusSlides(n + 2)}, timeToChange);
-    } else {
-        myTimer = setInterval(function(){plusSlides(n + 1)}, timeToChange);
-    }
+    // if (n === -1) {
+    //     myTimer[i] = setInterval(function(){plusSlides(n + 2, i)}, timeToChange);
+    // } else {
+    //     myTimer[i] = setInterval(function(){plusSlides(n + 1, i)}, timeToChange);
+    // }
 }
 
 //Controls the current slide and resets interval if needed
-function currentSlide(n) {
-    clearInterval(myTimer);
-    myTimer = setInterval(function(){plusSlides(n + 1)}, timeToChange);
-    showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//     clearInterval(myTimer);
+//     myTimer = setInterval(function(){plusSlides(n + 1)}, timeToChange);
+//     showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function showSlides(n, i) {
+    var slides = slideshows[i].getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex[i] = 1}
+    if (n < 1) {slideIndex[i] = slides.length}
+    for (var j = 0; j < slides.length; j++) {
+        slides[j].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex[i]-1].style.display = "grid";
 }
 
-pause = () => {
-    clearInterval(myTimer);
-}
+// pause = (i) => {
+//     clearInterval(myTimer[i]);
+// }
 
-resume = () =>{
-    clearInterval(myTimer);
-    myTimer = setInterval(function(){plusSlides(slideIndex)}, timeToChange);
-}
+// resume = (i) =>{
+//     clearInterval(myTimer[i]);
+//     myTimer[i] = setInterval(function(){plusSlides(slideIndex, i)}, timeToChange);
+// }
